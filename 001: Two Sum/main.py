268b8b54865af769113e2d7ac1e19407bb28f4da
Al Amin Chayan <al.amin.chayan@portonics.com>
2021-07-22 21:51:20 +0600

    add main file

---
+++ 001: Two Sum/main.py
@@ -0,0 +1,10 @@
+from solution_01 import Solution
+
+nums = [2, 7, 11, 15]
+target = 9
+
+solution = Solution()
+
+output = solution.twoSum(nums, target)
+
+print(output)
--- 001: Two Sum/solution_01.py
+++ 001: Two Sum/solution_01.py
@@ -1,5 +1,6 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+    def twoSum(self, nums: list, target: int) -> list:
         length = len(nums)
 
         enumerate_nums = enumerate(nums)
