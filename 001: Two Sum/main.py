aa9596756d0709893d07e1053a957713c472ade2
Al Amin Chayan <al.amin.chayan@portonics.com>
2021-07-23 11:41:29 +0600

    write test code

--- 001: Two Sum/main.py
+++ 001: Two Sum/main.py
@@ -1,10 +1,35 @@
 from solution_01 import Solution
 
-nums = [2, 7, 11, 15]
-target = 9
 
-solution = Solution()
+def test_example1():
+    nums = [2, 7, 11, 15]
+    target = 9
 
-output = solution.twoSum(nums, target)
+    solution = Solution()
 
-print(output)
+    assert solution.twoSum(nums, target) == [0, 1], "Should be [0, 1]"
+
+
+def test_example2():
+    nums = [3, 2, 4]
+    target = 6
+
+    solution = Solution()
+
+    assert solution.twoSum(nums, target) == [1, 2], "Should be [1, 2]"
+
+
+def test_example3():
+    nums = [3, 3]
+    target = 6
+
+    solution = Solution()
+
+    assert solution.twoSum(nums, target) == [0, 1], "Should be [0, 1]"
+
+
+if __name__ == "__main__":
+    test_example1()
+    test_example2()
+    test_example3()
+    print("Everything passed")
